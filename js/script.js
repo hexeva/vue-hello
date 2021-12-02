3b5362210a93e2036fcb3ceea8c3ee17d964a2fc
hexeva <patrick79.g@gmail.com>
2021-12-02 15:29:32 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 Vue.config.devtools = true;
 
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const app = new Vue({
 
     el: '#root',
-    data:{},
+    data:{
+        titleText:'Ciao io sono un corvo',
+        documentImg:'https://th.bing.com/th/id/R.7dede6593e6f550e34cfcd6814235d9d?rik=yCoDl1Por74a4Q&pid=ImgRaw&r=0',
+        titleClass:'violet'
+    },
     method:{}
 
 
